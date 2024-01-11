662d60b1b38bd5cab768b9818616b289ad3fc856
ponlaput <peechud@hotmail.com>
2024-01-11 21:53:07 +0700

    commit

--- src/app/chatbox/page.tsx
+++ src/app/chatbox/page.tsx
@@ -1,7 +1,7 @@
 export default async function ChatPage() {
     return(
         <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
-            <div className="col-span-full text-center">{"Test1"}</div>
+            <div className="col-span-full text-center">{"Test"}</div>
         </div>
     )
 }
