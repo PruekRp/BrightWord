8b63995bb50d9104ba4318edbe14f405fdbe974d
ponlaput <peechud@hotmail.com>
2024-01-16 03:09:44 +0700

    Create layout.tsx

---
+++ src/app/write/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'New Post | BrightWord',
+    description: 'write for BrightWord',
+  }
+  
+
+export default function WriteLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section>
+       
+        {children}
+      </section>
+    )
+  }
